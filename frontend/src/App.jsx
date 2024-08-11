97e0d4c9230dec32712a4bbba15428e7545c9019
Jackie Dai <jackydai12.21@gmail.com>
2024-08-11 12:51:47 -0400

    added useMemo hook to avoid re-render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
 import styled from "styled-components";
-import React from "react";
+import React, { useMemo, useState } from "react";
 import bg from "./img/bg.jpg";
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./Components/Orb/Orb";
 import Navigation from "./Components/Navigation/Navigation";
 
 function App() {
+  const [active, setActive] = useState(1);
+
+  const orbMemo = useMemo(() => {
+    return <Orb />;
+  }, []);
+
   return (
     <AppStyled bg={bg} className="App">
-      <Orb />
+      {orbMemo}
       <MainLayout>
-        <Navigation />
-        <h1>How long until I have 3 million in equity?</h1>
+        <Navigation active={active} setActive={setActive} />
+        <h1>How long until I have 3 million dollars?</h1>
       </MainLayout>
     </AppStyled>
   );
