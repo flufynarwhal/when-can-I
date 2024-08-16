b8899301ce510b50cfbf51daf34bafa06621fe15
Jackie Dai <jackydai12.21@gmail.com>
2024-08-15 22:18:07 -0400

    added button component and styling

--- frontend/src/Components/Form/Form.jsx
+++ frontend/src/Components/Form/Form.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useGlobalContext } from "../../context/globalContext";
+import Button from "../Button/Button";
+import { plus } from "../../utils/icons.jsx";
 
 function Form() {
   const { addIncome } = useGlobalContext();
@@ -19,6 +21,7 @@ function Form() {
 
   const handleInput = (name) => (e) => {
     setInputState({ ...inputState, [name]: e.target.value });
+    setError("");
   };
 
   const handleSubmit = (e) => {
@@ -58,7 +61,7 @@ function Form() {
           }}
         />
       </div>
-      <div className="selects input-control">
+      <div className="selects input--control">
         <select
           required
           value={category}
@@ -83,7 +86,7 @@ function Form() {
         <textarea
           name="description"
           value={description}
-          placeholder="Add A Reference "
+          placeholder="Add a description"
           id="description"
           cols="30"
           rows="4"
@@ -91,12 +94,67 @@ function Form() {
         ></textarea>
       </div>
       <div className="submit--btn">
-        <button>Add Income</button>
+        <Button
+          name={"Add Income"}
+          icon={plus}
+          bPad={".8rem 1.6rem"}
+          bRad={"30px"}
+          bg={"var(--color-accent"}
+          color={"#fff"}
+        />
       </div>
     </FormStyled>
   );
 }
 
-const FormStyled = styled.form``;
+const FormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  input,
+  textarea,
+  select {
+    font-family: inherit;
+    font-size: inherit;
+    outline: none;
+    border: none;
+    padding: 0.5rem 1rem;
+    border-radius: 5px;
+    border: 2px solid #fff;
+    background: transparent;
+    resize: none;
+    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+    color: rgba(34, 34, 96, 0.9);
+    &::placeholder {
+      color: rgba(34, 34, 96, 0.4);
+    }
+  }
+  .input--control {
+    input {
+      width: 100%;
+    }
+  }
+
+  .selects {
+    display: flex;
+    justify-content: flex-end;
+    select {
+      color: rgba(34, 34, 96, 0.4);
+      &:focus,
+      &:active {
+        color: rgba(34, 34, 96, 1);
+      }
+    }
+  }
+
+  .submit--btn {
+    button {
+      box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+      &:hover {
+        background: var(--color-green) !important;
+      }
+    }
+  }
+`;
 
 export default Form;
--- frontend/src/Components/Orb/Orb.jsx
+++ frontend/src/Components/Orb/Orb.jsx
@@ -6,6 +6,7 @@ function Orb() {
   const { width, height } = useWindowSize();
 
   console.log(width, height);
+
   const moveOrb = keyframes`
         0%{
             transform: translate(0, 0);
@@ -22,7 +23,7 @@ function Orb() {
     width: 70vh;
     height: 70vh;
     position: absolute;
-    border-radius: 60%;
+    border-radius: 50%;
     margin-left: -37vh;
     margin-top: -37vh;
     background: linear-gradient(180deg, #f56692 0%, #f2994a 100%);
